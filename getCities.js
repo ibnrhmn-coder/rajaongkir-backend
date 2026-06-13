7a12a1a5d985ac1102d0c7e1b6c9b76b132add51
ibnrhmn-coder <ibnrhmn@gmail.com>
2026-06-14 02:50:40 +0700

    Update getCities.js

--- getCities.js
+++ getCities.js
@@ -1,1 +1,14 @@
+// api/getCities.js
+export default async function handler(req, res) {
+  try {
+    const response = await fetch("https://api.rajaongkir.com/starter/city", {
+      headers: { key: process.env.RAJAONGKIR_API_KEY }
+    });
 
+    const data = await response.json();
+    // Ambil hasil daftar kota
+    res.status(200).json(data.rajaongkir.results);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+}
