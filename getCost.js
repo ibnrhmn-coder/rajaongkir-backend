2d4c158224bd0abad0a4a45d5703f3ef91968066
ibnrhmn-coder <ibnrhmn@gmail.com>
2026-06-14 02:39:58 +0700

    Update getCost.js

--- getCost.js
+++ getCost.js
@@ -1,1 +1,21 @@
+// api/getCost.js
+export default async function handler(req, res) {
+  try {
+    const { origin, destination, weight, courier } = req.body;
 
+    const response = await fetch("https://api.rajaongkir.com/starter/cost", {
+      method: "POST",
+      headers: {
+        key: process.env.RAJAONGKIR_API_KEY,
+        "content-type": "application/x-www-form-urlencoded"
+      },
+      body: new URLSearchParams({ origin, destination, weight, courier })
+    });
+
+    const data = await response.json();
+    // Ambil hasil ongkos kirim
+    res.status(200).json(data.rajaongkir.results);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+}
